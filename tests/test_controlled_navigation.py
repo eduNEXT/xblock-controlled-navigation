114caf121aff9de0a0e7fdbfa9344c7528527096
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-04-12 18:20:42 -0500

    chore: remove unused test

--- tests/test_controlled_navigation.py
+++ tests/test_controlled_navigation.py
@@ -28,33 +28,6 @@ class TestXblockControlledNavegation(TestCase):
             scope_ids=ScopeIds("1", "2", "3", "4"),
         )
 
-    def test_student_view_without_children(self):
-        """Render the student view without children.
-        Expected result: an empty div.
-        """
-        self.block.children = []
-
-        fragment = self.block.student_view({})
-
-        self.assertEqual(
-            fragment.content.replace("\n", "").replace(" ", ""),
-            '<divclass="controlled_navigation_block"></div>',
-        )
-
-    def test_student_view_with_children(self):
-        """Render the student view with children.
-        Expected result: a div with the children content.
-        """
-        self.block.children = ["child1"]
-        self.runtime.get_block = Mock(return_value=self.child_block)
-
-        fragment = self.block.student_view({})
-
-        self.assertEqual(
-            fragment.content.replace("\n", "").replace(" ", ""),
-            '<divclass="controlled_navigation_block">MyXBlock:countisnow0</div>',
-        )
-
     def test_author_view_root(self):
         """Render the author view with the root block.
         Expected result: a div with the children content.
