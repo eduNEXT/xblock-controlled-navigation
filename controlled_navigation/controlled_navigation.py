2a3c461d61e980eed62eede1a46824df3c036550
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-04-15 14:36:01 -0500

    chore: fix quality checks

--- controlled_navigation/controlled_navigation.py
+++ controlled_navigation/controlled_navigation.py
@@ -135,14 +135,14 @@ class XBlockControlledNavigation(
 
     def studio_view(self, context):
         """
-        Render a form for editing this XBlock
+        Render a form for editing this XBlock.
         """
         fragment = Fragment()
         context = {"fields": []}
 
         # Build a list of all the fields that can be edited:
         for field_name in self.editable_fields:
-            field = self.fields[field_name]
+            field = self.fields[field_name]  # pylint: disable=unsubscriptable-object
             assert field.scope in (Scope.content, Scope.settings), (
                 "Only Scope.content or Scope.settings fields can be used with "
                 "StudioEditableXBlockMixin. Other scopes are for user-specific data and are "
