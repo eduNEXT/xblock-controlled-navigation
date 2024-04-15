c3422a4e54190c4df3a4791cef63b27ee47429c6
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-04-14 23:06:44 -0500

    chore: fix quality checks

--- controlled_navigation/utils.py
+++ controlled_navigation/utils.py
@@ -5,6 +5,6 @@ Utilities for controlled navigation xblock.
 
 def _(text):
     """
-    Dummy `gettext` replacement to make string extraction tools scrape strings marked for translation.
+    Make '_' a no-op so we can scrape strings.
     """
     return text
